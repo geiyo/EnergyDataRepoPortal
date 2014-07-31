84582baa4fc6a1039ce774be7a66b642f0177f0f
Greg Iyo Jr <gregiyo@gmail.com>
2014-07-31 13:48:25 +0800

    Installed GWT Beta Version

---
+++ sites/all/modules/custom/location_map/js/loadmap.js
@@ -0,0 +1,11 @@
+nokia.Settings.set("app_id", "4Uk6wOxIQOjdE9NKrIHp");
+        nokia.Settings.set("app_code", "OACnU6nT5yVcO1WlcxAIZQ");
+        
+var map = new nokia.maps.map.Display(
+        	document.getElementById("mapContainer"), {
+        		// Zoom level for the map
+        		zoomLevel: 10,
+        		// Map center coordinates
+        		center: [52.51, 13.4]
+        	}
+        );
